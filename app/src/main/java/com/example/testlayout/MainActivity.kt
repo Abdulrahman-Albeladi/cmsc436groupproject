e02b9d51a73bc2256279051da415e3b18115bad9
Kevin Wu <kwu12348@umd.edu>
2024-06-24 17:25:15 -0400

    First NavBar commit

---
+++ app/src/main/java/com/example/testlayout/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.testlayout
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.testlayout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(RunFragment())
+
+        binding.bottomNavBar.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.run_nav -> replaceFragment(RunFragment())
+                R.id.group_nav -> replaceFragment(GroupFragment())
+                R.id.map_nav -> replaceFragment(MapFragment())
+                else -> {
+
+                }
+            }
+
+            return@setOnItemSelectedListener true
+        }
+    }
+
+    fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+    }
+}
